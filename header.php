2a95d4f997683e6d3f78de9e6667256c8a295b9f
Steven Soehl <steven@soehlinteractive.com>
2014-11-21 07:20:12 -0500

    Update header.php

--- header.php
+++ header.php
@@ -62,7 +62,7 @@
 
         
 
-		<script src="<?php echo get_template_directory_uri(); ?>/js/bootstrap.min.js"></script>
+		
 
         
 
@@ -95,7 +95,7 @@
     <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
       <div class="container">
         <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false" aria-controls="navbar">
             <span class="sr-only">Toggle navigation</span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
@@ -103,28 +103,19 @@
           </button>
            <a class="navbar-brand" href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a>
         </div>
-        <div id="navbar" class="collapse navbar-collapse">
-           <div class="navbar-collapse collapse navbar-ex1-collapse">
-
-        
-              <?php /* Primary navigation */
-                
-                wp_nav_menu( array(
-                
-                  'menu' => __( 'The Main Menu', 'bootztheme' ),
-                
-                  'depth' => 2,
-                
-                  'container' => false,
-                
-                  'menu_class' => 'nav navbar-nav ',
-                
-                  //Process nav menu using our custom nav walker
-                
-                  'walker' => new wp_bootstrap_navwalker())
-                
-                ); ?>
-        </div><!--/.nav-collapse -->
+         <?php
+            wp_nav_menu( array(
+                'menu' => __( 'The Main Menu', 'bootztheme' ),
+                'theme_location'    => 'main-nav',
+                'depth'             => 2,
+                'container'         => 'div',
+                'container_class'   => 'collapse navbar-collapse',
+                  'container_id'      => 'bs-example-navbar-collapse-1',
+                'menu_class'        => 'nav navbar-nav',
+                'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
+                'walker'            => new wp_bootstrap_navwalker())
+            );
+        ?>
       </div>
     </nav>
     <?php get_template_part('inc/jumbotron'); ?>
